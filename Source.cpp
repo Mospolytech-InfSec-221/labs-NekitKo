eecd01f6cf8291feb0cda41ab926e55c5b0b4e87
Nik <115219256+NekitKo@users.noreply.github.com>
2022-12-30 15:22:06 +0300

    lab 6.6

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,15 @@
 ﻿#include <iostream>
-#include <limits>
-#include <string>
-#include <list>
+#include <fstream>
+#include <map>
 
 #define russianIO(x)	system("chcp 1251>nul");\
 						x;\
 						system("chcp 65001>nul")
 
+using std::ifstream;
+using std::ofstream;
 using std::string;
-using std::list;
+using std::map;
 using std::cout;
 using std::cin;
 using std::endl;
@@ -18,42 +19,81 @@ void fix() {
 	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-bool compare(string st1, string st2) {
-	int num1 = std::stoi(st1.substr(0, st1.find(" ")).erase(st1.find("-"), 1));
-	int num2 = std::stoi(st2.substr(0, st2.find(" ")).erase(st2.find("-"), 1));
+char toLower(char symb) {
+	if ((int)symb < -32 && (int)symb > -65 || (int)symb < 91 && (int)symb > 64) return (char)((int)symb + 32);
+	else return symb;
+}
 
-	return num1 < num2;
+string to_lower(string word) {
+	for (size_t i = 0; i < word.size(); i++) {
+		word[i] = toLower(word[i]);
+	}
+	return word;
+}
+
+bool isLetter(char symb) {
+	if ((symb >= (char)65 && symb <= (char)90) || (symb >= (char)97 && symb <= (char)122)) return true;
+	if (symb >= (char)-64 && symb <= (char)-1) return true;
+	else return false;
+}
+
+map<string, int> countWords(string filename, int& wordsCount) {
+	map<string, int> words;
+	string word;
+	string error;
+	ifstream input(filename);
+	if (input.is_open()) {
+		while (input >> word) {
+			if (isLetter(word[0])) {
+				while (!isLetter(word[word.length() - 1])) word.erase(word.length() - 1);
+				word = to_lower(word);
+				if (words.find(word) == words.end()) words[word] = 1;
+				else words[word]++;
+				wordsCount++;
+			}
+		}
+		return words;
+	}
+	else error = "Файла с указанным именем не существует";
+	input.close();
+	wordsCount = 0;
+	if (!error.empty()) throw std::exception(error.data());
 }
 
 int main() {
 	system("chcp 65001>nul");
-	string s;
-	string groupname = "";
-	list<string> students;
+	map<string, int> words;
+	ofstream out;
+	string filename = "";
+	int count = 0;
 	int option;
 	while (true) {
 		cout << "Выберите действие:\n"
-			<< "1. Добавить запись\n"
-			<< "2. Вывести студентов по группам\n"
+			<< "1. Выбрать файл\n"
+			<< "2. Показать статистику\n"
 			<< "3. Выход\n"
 			<< "Ваш выбор: ";
 		cin >> option;
 		switch (option) {
 		case 1:
 			fix();
-			cout << "Введите группу и фамилию студента через пробел: ";
-			russianIO(std::getline(cin, s));
-			students.push_back(s);
+			cout << "Введите полное имя файла: ";
+			russianIO(cin >> filename);
+			try {
+				words = countWords(filename, count);
+			}
+			catch (const std::exception& e) {
+				cout << e.what();
+			}
 			break;
 		case 2:
-			students.sort(compare);
-			russianIO(for (auto student : students) {
-				if (groupname != student.substr(0, student.find(" "))) {
-					groupname = student.substr(0, student.find(" "));
-					cout << groupname << endl;
-				}
-				cout << "\t\t" << student.substr(student.find(" ")) << endl;
-			});
+			out.open("D:\\results.txt");
+			if (out.is_open()) {
+				out << "Всего слов: " << count << endl;
+				for (auto word : words) out << word.first << ": " << word.second << ", " << word.second / (double)count * 100.0 << "%\n";
+				out.close();
+			}
+			cout << "Запись в файл окончена. Ознакомиться с результатами вы сможете по следующему пути: D:\\results.txt\n";
 			break;
 		case 3:
 			return 0;
